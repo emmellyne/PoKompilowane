0472ff8db951ce652b04273a43d1b338340810d6
Maya <maja.wymyslowska@gmail.com>
2023-04-20 03:53:02 +0200

    handling malicious code execution (124)

--- src/index.tsx
+++ src/index.tsx
@@ -7,8 +7,8 @@ import { fetchPlugin } from './plugins/fetch-plugin';
 
 const App = () => {
   const ref = useRef<any>();
+  const iframe = useRef<any>();
   const [input, SetInput] = useState('');
-  const [code, setCode] = useState('');
 
 
    const startService = async () => {
@@ -26,6 +26,8 @@ const App = () => {
     if (!ref.current) {
       return;
     }
+
+    iframe.current.srcdoc = html;
     
     const result = await ref.current.build({
       entryPoints: ['index.js'],
@@ -41,17 +43,39 @@ const App = () => {
 
     // console.log(result)
     
-    setCode(result.outputFiles[0].text);
+    // setCode(result.outputFiles[0].text);
+    iframe.current.contentWindow.postMessage(result.outputFiles[0].text, '*')
   };
 
-  return <div>
+  const html = `
+    <html>
+      <head></head>
+      <body>
+        <div id="root"></div>
+        <script>
+          window.addEventListener('message', (event) => {
+            try {
+              eval(event.data);
+            } catch (err) {
+              const root = document.querySelector('#root');
+              root.innerHTML = '<div style="color: red;"><h4>Runtime Error</h4>' + err + '</div>'
+              console.error(err);
+            }
+          }, false);
+        </script>
+      </body>
+    </html>
+    `;
+
+  return (<div>
     <textarea value={input} onChange={e => SetInput(e.target.value)}>
     </textarea>
       <div>
         <button onClick={onClick}>Submit</button>
       </div>
-      <pre>{code}</pre>
-      </div>;
+      <iframe title="preview" ref={iframe} sandbox="allow-scripts" srcDoc={html} />
+      </div>
+  );
 };
 
 ReactDOM.render(<App />, document.querySelector('#root'));
