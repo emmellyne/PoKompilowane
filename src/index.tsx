5f06fe7118fb882f64481fd9c69d0504449e14ce
weronikajedras <260478@student.pwr.edu.pl>
2023-04-27 08:07:41 +0200

    bulma change

--- src/index.tsx
+++ src/index.tsx
@@ -1,4 +1,4 @@
-import 'bulmaswatch/superhero/bulmaswatch.min.css';
+import 'bulmaswatch/darkly/bulmaswatch.min.css';
 import ReactDOM from 'react-dom';
 import { Provider } from 'react-redux';
 import { store } from './state';
@@ -7,11 +7,17 @@ import TextEditor from './components/text-editor';
 
 const App = () => {
   return (
+    /*
     <Provider store={store}>
       <div>
         <TextEditor />
       </div>
     </Provider>
+    */
+    <div>
+    <TextEditor />
+    <CodeCell />
+    </div>
     
   );
 };
